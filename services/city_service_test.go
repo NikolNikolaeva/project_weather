13e01d456641bc66d03a051dd49ecbcd67576aa6
nikol_lea <nikol_lea@abv.bg>
2024-06-28 12:34:55 +0300

    separate tests

--- services/city_service_test.go
+++ services/city_service_test.go
@@ -20,7 +20,27 @@ import (
 	mock_services "github.com/NikolNikolaeva/project_weather/generated/go-mocks/services"
 )
 
-func Test_GetById(t *testing.T) {
+func TestAPIService_GetById_CityNotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	cityID := "1"
+
+	mockRepo.EXPECT().FindByID(cityID).Return(nil, errors.New("city not found"))
+
+	response, err := service.GetById(context.Background(), cityID)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusInternalServerError, response.Code)
+	assert.Equal(t, "city not found", response.Body)
+}
+
+func TestAPIService_GetById_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -44,36 +64,32 @@ func Test_GetById(t *testing.T) {
 		Longitude: "0",
 	}
 
-	t.Run("City Not Found", func(t *testing.T) {
-		mockRepo.EXPECT().FindByID(cityID).Return(nil, errors.New("city not found"))
+	mockRepo.EXPECT().FindByID(cityID).Return(modelCity, nil)
+	mockConverter.EXPECT().ConvertModelCityToApiCity(modelCity).Return(apiCity)
 
-		response, err := service.GetById(context.Background(), cityID)
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusInternalServerError, response.Code)
-		assert.Equal(t, "city not found", response.Body)
-	})
-
-	t.Run("Success", func(t *testing.T) {
-		mockRepo.EXPECT().FindByID(cityID).Return(modelCity, nil)
-		mockConverter.EXPECT().ConvertModelCityToApiCity(modelCity).Return(apiCity)
-
-		response, err := service.GetById(context.Background(), cityID)
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Equal(t, apiCity, response.Body)
-	})
-
-	t.Run("Require id", func(t *testing.T) {
-
-		response, err := service.GetById(context.Background(), "")
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
-		assert.Equal(t, "id is required", response.Body)
-	})
-
+	response, err := service.GetById(context.Background(), cityID)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, apiCity, response.Body)
 }
 
-func Test_DeleteById(t *testing.T) {
+func TestAPIService_GetById_RequireId(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	response, err := service.GetById(context.Background(), "")
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
+	assert.Equal(t, "id is required", response.Body)
+}
+
+func TestAPIService_DeleteById_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -97,37 +113,53 @@ func Test_DeleteById(t *testing.T) {
 		Longitude: "0",
 	}
 
-	t.Run("Success", func(t *testing.T) {
-		mockRepo.EXPECT().FindByID(cityID).Return(modelCity, nil)
-		mockRepo.EXPECT().DeleteByID(cityID).Return(gen.ResultInfo{}, nil)
-		mockConverter.EXPECT().ConvertModelCityToApiCity(modelCity).Return(apiCity)
+	mockRepo.EXPECT().FindByID(cityID).Return(modelCity, nil)
+	mockRepo.EXPECT().DeleteByID(cityID).Return(gen.ResultInfo{}, nil)
+	mockConverter.EXPECT().ConvertModelCityToApiCity(modelCity).Return(apiCity)
 
-		response, err := service.DeleteById(context.Background(), cityID)
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Equal(t, apiCity, response.Body)
-	})
-
-	t.Run("City Not Found", func(t *testing.T) {
-		mockRepo.EXPECT().FindByID(cityID).Return(nil, errors.New("city not found"))
-
-		response, err := service.DeleteById(context.Background(), cityID)
-		assert.Error(t, err)
-		assert.Equal(t, http.StatusNotFound, response.Code)
-		assert.Equal(t, "City not found", response.Body)
-	})
-
-	t.Run("Require id", func(t *testing.T) {
-
-		response, err := service.DeleteById(context.Background(), "")
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusBadRequest, response.Code)
-		assert.Equal(t, "id is required", response.Body)
-	})
-
+	response, err := service.DeleteById(context.Background(), cityID)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, apiCity, response.Body)
 }
 
-func Test_GetAll(t *testing.T) {
+func TestAPIService_DeleteById_CityNotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	cityID := "1"
+
+	mockRepo.EXPECT().FindByID(cityID).Return(nil, errors.New("city not found"))
+
+	response, err := service.DeleteById(context.Background(), cityID)
+	assert.Error(t, err)
+	assert.Equal(t, http.StatusNotFound, response.Code)
+	assert.Equal(t, "City not found", response.Body)
+}
+
+func TestAPIService_DeleteById_RequireId(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	response, err := service.DeleteById(context.Background(), "")
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusBadRequest, response.Code)
+	assert.Equal(t, "id is required", response.Body)
+}
+
+func TestAPIService_GetAll_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -166,31 +198,37 @@ func Test_GetAll(t *testing.T) {
 		},
 	}
 
-	t.Run("Success", func(t *testing.T) {
-		mockRepo.EXPECT().GetAll().Return(modelCities, nil)
+	mockRepo.EXPECT().GetAll().Return(modelCities, nil)
 
-		for i, city := range modelCities {
-			mockConverter.EXPECT().ConvertModelCityToApiCity(city).Return(apiCities[i])
-		}
+	for i, city := range modelCities {
+		mockConverter.EXPECT().ConvertModelCityToApiCity(city).Return(apiCities[i])
+	}
 
-		response, err := service.GetAll(context.Background())
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Equal(t, apiCities, response.Body)
-	})
-
-	t.Run("Error", func(t *testing.T) {
-		mockRepo.EXPECT().GetAll().Return(nil, errors.New("db error"))
-
-		response, err := service.GetAll(context.Background())
-		assert.Error(t, err, "db error")
-		assert.Equal(t, http.StatusInternalServerError, response.Code)
-		assert.Nil(t, response.Body)
-
-	})
+	response, err := service.GetAll(context.Background())
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, apiCities, response.Body)
 }
 
-func Test_Register(t *testing.T) {
+func TestAPIService_GetAll_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	mockRepo.EXPECT().GetAll().Return(nil, errors.New("db error"))
+
+	response, err := service.GetAll(context.Background())
+	assert.Error(t, err, "db error")
+	assert.Equal(t, http.StatusInternalServerError, response.Code)
+	assert.Nil(t, response.Body)
+}
+
+func TestAPIService_Register_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -219,43 +257,61 @@ func Test_Register(t *testing.T) {
 		Lon: 0,
 	}
 
-	t.Run("Success", func(t *testing.T) {
+	forecast := &weatherapi.Forecast{}
 
-		forecast := &weatherapi.Forecast{}
+	mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity)
+	mockHandler.EXPECT().HandleCityData(modelCity.Name, gomock.Any()).Return(location)
+	mockRepo.EXPECT().Register(modelCity).Return(registeredCity, nil)
+	mockConverter.EXPECT().ConvertModelCityToApiCity(registeredCity).Return(apiCity)
 
-		mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity)
-		mockHandler.EXPECT().HandleCityData(modelCity.Name, gomock.Any()).Return(location)
-		mockRepo.EXPECT().Register(modelCity).Return(registeredCity, nil)
-		mockConverter.EXPECT().ConvertModelCityToApiCity(registeredCity).Return(apiCity)
+	mockHandler.EXPECT().HandleForecast(modelCity.Name, int32(30), gomock.Any()).Return(forecast, nil).Do(func(string, int32, string) {
+		wg.Done()
+	}).Times(1)
 
-		mockHandler.EXPECT().HandleForecast(modelCity.Name, int32(30), gomock.Any()).Return(forecast, nil).Do(func(string, int32, string) {
-			wg.Done()
-		}).Times(1)
+	response, err := service.Register(context.Background(), *apiCity)
 
-		response, err := service.Register(context.Background(), *apiCity)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, response.Code)
+	assert.Equal(t, apiCity, response.Body)
 
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusCreated, response.Code)
-		assert.Equal(t, apiCity, response.Body)
-
-		wg.Wait()
-	})
-
-	t.Run("Error", func(t *testing.T) {
-
-		mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity).Times(1)
-		mockHandler.EXPECT().HandleCityData(modelCity.Name, gomock.Any()).Return(location).Times(1)
-		mockRepo.EXPECT().Register(modelCity).Return(&model.City{}, errors.New("db error")).Times(1)
-
-		response, err := service.Register(context.Background(), *apiCity)
-
-		assert.Error(t, err, "db error")
-		assert.Equal(t, http.StatusInternalServerError, response.Code)
-		assert.Nil(t, response.Body)
-	})
+	wg.Wait()
 }
 
-func Test_UpdateByID(t *testing.T) {
+func TestAPIService_Register_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	apiCity := &api.City{
+		Name: "Test City",
+	}
+	modelCity := &model.City{
+		Name:      "Test City",
+		Latitude:  "0",
+		Longitude: "0",
+	}
+	location := &weatherapi.Location{
+		Lat: 0,
+		Lon: 0,
+	}
+
+	mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity).Times(1)
+	mockHandler.EXPECT().HandleCityData(modelCity.Name, gomock.Any()).Return(location).Times(1)
+	mockRepo.EXPECT().Register(modelCity).Return(&model.City{}, errors.New("db error")).Times(1)
+
+	response, err := service.Register(context.Background(), *apiCity)
+
+	assert.Error(t, err, "db error")
+	assert.Equal(t, http.StatusInternalServerError, response.Code)
+	assert.Nil(t, response.Body)
+}
+
+func TestAPIService_UpdateByID_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -275,24 +331,39 @@ func Test_UpdateByID(t *testing.T) {
 	updatedCity := modelCity
 	updatedCity.ID = cityID
 
-	t.Run("Success", func(t *testing.T) {
-		mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity)
-		mockRepo.EXPECT().UpdateByID(cityID, modelCity).Return(updatedCity, nil)
-		mockConverter.EXPECT().ConvertModelCityToApiCity(updatedCity).Return(apiCity)
+	mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity)
+	mockRepo.EXPECT().UpdateByID(cityID, modelCity).Return(updatedCity, nil)
+	mockConverter.EXPECT().ConvertModelCityToApiCity(updatedCity).Return(apiCity)
 
-		response, err := service.UpdateByID(context.Background(), cityID, *apiCity)
-		assert.Nil(t, err)
-		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Equal(t, apiCity, response.Body)
-	})
+	response, err := service.UpdateByID(context.Background(), cityID, *apiCity)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, apiCity, response.Body)
+}
 
-	t.Run("Error", func(t *testing.T) {
-		mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity)
-		mockRepo.EXPECT().UpdateByID(cityID, modelCity).Return(nil, errors.New("db error"))
+func TestAPIService_UpdateByID_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-		response, err := service.UpdateByID(context.Background(), cityID, *apiCity)
-		assert.Error(t, err, "db error")
-		assert.Equal(t, http.StatusInternalServerError, response.Code)
-		assert.Nil(t, response.Body)
-	})
+	mockRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockConverter := mock_resources.NewMockConverterI(ctrl)
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
+
+	service := NewAPIService(mockRepo, mockConverter, mockHandler, &config.ApplicationConfiguration{})
+
+	cityID := "1"
+	apiCity := &api.City{
+		Name: "Updated City",
+	}
+	modelCity := &model.City{
+		Name: "Updated City",
+	}
+
+	mockConverter.EXPECT().ConvertApiCityToModelCity(apiCity).Return(modelCity)
+	mockRepo.EXPECT().UpdateByID(cityID, modelCity).Return(nil, errors.New("db error"))
+
+	response, err := service.UpdateByID(context.Background(), cityID, *apiCity)
+	assert.Error(t, err, "db error")
+	assert.Equal(t, http.StatusInternalServerError, response.Code)
+	assert.Nil(t, response.Body)
 }
--- services/weather_api_servicer_test.go
+++ services/weather_api_servicer_test.go
@@ -3,17 +3,19 @@ package services
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+
 	"github.com/NikolNikolaeva/project_weather/config"
 	"github.com/NikolNikolaeva/project_weather/generated/dao/model"
 	mock_repositories "github.com/NikolNikolaeva/project_weather/generated/go-mocks/repositories"
 	mock_services "github.com/NikolNikolaeva/project_weather/generated/go-mocks/services"
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/mock/gomock"
-	"log"
-	"testing"
 )
 
-func TestWeatherService_StartFetching(t *testing.T) {
+func TestWeatherService_StartFetching_ErrorFetchingCities(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -27,39 +29,50 @@ func TestWeatherService_StartFetching(t *testing.T) {
 
 	service := NewWeatherService(mockDataGetter, mockCityRepo, mockForecastRepo, configuration, mockHandler)
 
-	t.Run("Error fetching cities", func(t *testing.T) {
-		mockCityRepo.EXPECT().GetAll().Return(nil, fmt.Errorf("failed to fetch cities")).Times(1)
+	mockCityRepo.EXPECT().GetAll().Return(nil, fmt.Errorf("failed to fetch cities")).Times(1)
 
-		var buf bytes.Buffer
-		log.SetOutput(&buf)
-		defer func() {
-			log.SetOutput(nil)
-		}()
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(nil)
+	}()
 
-		service.StartFetching()
+	service.StartFetching()
 
-		logOutput := buf.String()
-		assert.Contains(t, logOutput, "Error fetching cities")
-	})
+	logOutput := buf.String()
+	assert.Contains(t, logOutput, "Error fetching cities")
+}
 
-	t.Run("Error fetching forecast", func(t *testing.T) {
-		mockCityRepo.EXPECT().GetAll().Return([]*model.City{
-			{
-				Name: "City test",
-			},
-		}, nil).Times(1)
-		mockHandler.EXPECT().HandleForecast("City test", int32(30), configuration.CredFile).
-			Return(nil, fmt.Errorf("failed to fetch forecast")).Times(1)
+func TestWeatherService_StartFetching_ErrorFetchingForecast(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-		var buf bytes.Buffer
-		log.SetOutput(&buf)
-		defer func() {
-			log.SetOutput(nil)
-		}()
+	mockDataGetter := mock_services.NewMockWeatherDataGetter(ctrl)
+	mockCityRepo := mock_repositories.NewMockCityRepo(ctrl)
+	mockForecastRepo := mock_repositories.NewMockForecastRepo(ctrl)
+	configuration := &config.ApplicationConfiguration{
+		CredFile: "credentials.json",
+	}
+	mockHandler := mock_services.NewMockWeatherAPIClient(ctrl)
 
-		service.StartFetching()
+	service := NewWeatherService(mockDataGetter, mockCityRepo, mockForecastRepo, configuration, mockHandler)
 
-		logOutput := buf.String()
-		assert.Contains(t, logOutput, "Error fetching forecast for city")
-	})
+	mockCityRepo.EXPECT().GetAll().Return([]*model.City{
+		{
+			Name: "City test",
+		},
+	}, nil).Times(1)
+	mockHandler.EXPECT().HandleForecast("City test", int32(30), configuration.CredFile).
+		Return(nil, fmt.Errorf("failed to fetch forecast")).Times(1)
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(nil)
+	}()
+
+	service.StartFetching()
+
+	logOutput := buf.String()
+	assert.Contains(t, logOutput, "Error fetching forecast for city")
 }
